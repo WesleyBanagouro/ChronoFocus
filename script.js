82320258966951c0a405b2e7acba778a73c33c50
WesleyBanagouro <wesley.banagouro@outlook.com>
2024-05-05 20:42:12 -0300

    adicionando interatividade de menu

--- script.js
+++ script.js
@@ -1,14 +1,86 @@
-var inMinutosFoco = document.getElementById("minutos-foco")
-function minutos() {
-    console.log(inMinutosFoco.value)
-}
+/*Minutos foco */
 
-var btnComecar = document.getElementById("btnComecar");
-btnComecar.addEventListener("click", minutos)
+var inMinutosFoco = document.getElementById("foco-set")
+var valorInMinutosFoco = parseInt(inMinutosFoco.value)
 
-function aumentarFoco() {
-    return inMinutosFoco.value += 1;
-}
+var avisoPreenchimento = document.getElementById("aviso-preenchimento")
 
-var btnAumentar = document.getElementById("btnAumentar")
-btnAumentar.addEventListener("click", aumentarFoco)
+var btnComecar = document.getElementById("btnComecar")
+var campoComecar = document.getElementById("comecar")
+
+var btnAumentarFoco = document.getElementById("btnAumentarFoco")
+var btnDiminuirFoco = document.getElementById("btnDiminuirFoco")
+
+btnDiminuirFoco.addEventListener("click", function() {
+    if(valorInMinutosFoco <= 0) {
+        return
+    }
+    valorInMinutosFoco -= 1;
+    return inMinutosFoco.value = valorInMinutosFoco
+})
+
+btnAumentarFoco.addEventListener("click", function() {
+    valorInMinutosFoco += 1;
+    return inMinutosFoco.value = valorInMinutosFoco
+})
+
+btnComecar.addEventListener("click", function(){
+    if(inMinutosFoco.value == 0 || inMinutosIntervalo.value == 0 || inCiclos.value == 0) {
+        avisoPreenchimento.style.color = 'yellow';
+        return
+    } else {
+        if (btnComecar.textContent === "Começar") {
+            avisoPreenchimento.style.color = "transparent";
+            btnComecar.textContent = "Pausar";
+            campoComecar.style.backgroundColor = "gray";
+          } else {
+            btnComecar.textContent = "Começar";
+            campoComecar.style.backgroundColor = "#FF2E2E"; // Original color for "Começar"
+          }
+    }
+})
+
+
+
+/*Minutos intervalo */
+
+var inMinutosIntervalo = document.getElementById("intervalo-set")
+var valorInMinutosIntervalo = parseInt(inMinutosIntervalo.value)
+
+var btnAumentarFoco = document.getElementById("btnAumentarIntervalo")
+var btnDiminuirFoco = document.getElementById("btnDiminuirIntervalo")
+
+btnDiminuirIntervalo.addEventListener("click", function() {
+    if(valorInMinutosIntervalo <= 0) {
+        return
+    }
+    valorInMinutosIntervalo -= 1;
+    return inMinutosIntervalo.value = valorInMinutosIntervalo
+})
+
+btnAumentarIntervalo.addEventListener("click", function() {
+    valorInMinutosIntervalo += 1;
+    return inMinutosIntervalo.value = valorInMinutosIntervalo
+})
+
+/*Numero de ciclos */
+
+var inCiclos = document.getElementById("ciclos-set")
+var valorInCiclos = parseInt(inCiclos.value)
+
+var btnDiminuirCiclos = document.getElementById("btnDiminuirCiclos")
+var btnAumentarCiclos = document.getElementById("btnAumentarCiclos")
+
+btnDiminuirCiclos.addEventListener("click", function() {
+    if(valorInCiclos <= 0) {
+        return
+    }
+    valorInCiclos -= 1;
+    return inCiclos.value = valorInCiclos
+})
+
+btnAumentarCiclos.addEventListener("click", function() {
+    valorInCiclos += 1;
+    return inCiclos.value = valorInCiclos
+})
+
