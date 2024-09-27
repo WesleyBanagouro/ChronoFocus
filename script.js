4651ec8925ce974aeeec1320f2c83c59e0765bdf
Wesley Banagouro <wesley.banagouro@outlook.com>
2024-09-27 16:11:59 +0000

    swiper

--- script.js
+++ script.js
@@ -551,7 +551,18 @@ function pause() {
     }
   });
 
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
   
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+  });
 
 
   
